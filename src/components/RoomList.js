9eca511d334d79efcb942ed5066408876e1b68a7
Sheena Gilliland <sheena.gilliland@gmail.com>
2018-09-08 12:33:38 -0700

    Attempt to get submit button working

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,42 +3,47 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-    rooms: []
+    rooms: [],
+    newRoomName: '',
   };
 
   this.roomsRef = this.props.firebase.database().ref('rooms');
-  }
+};
 
-  componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
-      const room = snapshot.val();
-      room.key = snapshot.key;
-      this.setState({
-        rooms: this.state.rooms.concat( room )
-      });
+componentDidMount() {
+  this.roomsRef.on('child_added', snapshot => {
+    const room = snapshot.val();
+    room.key = snapshot.key;
+    this.setState ({
+  rooms: this.state.rooms.concat( room )})
     });
+}
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({newRoomName: e.target.value});
   }
 
-  render(){
+ createRoom = (e) => {
+       this.roomsRef.push({
+       name: this.state.newRoomName
+      });
+     }
+
+  render() {
     return(
-      <table id='roomList'>
-        <thead>
-          <tr>
-            <th>Available Rooms</th>
-          </tr>
-        </thead>
-        <tbody>
+      <div className='sidebar'>
+      <div>
           {this.state.rooms.map( (room) =>
-          <tr key={room.key}>
-          <td>{room.name}</td>
-          </tr>
-        )}
-        </tbody>
-      </table>
-    );
-  }
+          <section key={room.key}>{room.name}</section>)}</div>
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.newRoomName} onChange={(e) => this.setState({newRoomName: e.target.value})}/>
+        <span>Room Name</span>
+        <input type="submit" />
+        </form>
+      </div>
+    )}
 }
 
 
